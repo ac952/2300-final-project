c99e1932dc9b08baff9d4970c27ea5f1701bb855
Jessica Zhao <jwz23@cornell.edu>
2018-04-27 18:25:41 -0400

    working on gallery

--- gallery.php
+++ gallery.php
@@ -1,1 +1,142 @@
+<?php
 
+include('includes/init.php');
+
+$current_page_id="index";
+
+const IMG_UPLOADS_PATH = "uploads/images/";
+
+echo "hello";
+
+if (isset($_POST['delete_photo'])) {
+  $delete_photo = TRUE;
+  $img_id = filter_input(INPUT_GET, 'delete_photo', FILTER_SANITIZE_STRING);
+  echo "found a delete_photo";
+  echo $img_id." is id";
+  }
+else {
+  echo " wtf";
+  $delete_photo = FALSE;
+  $img_id = NULL;
+}
+
+//adding a photo
+const MAX_FILE_SIZE = 1000000;
+const IMG_UPLOADS_PATH = "uploads/images/";
+
+//performing upload
+if (isset($_POST["submit_upload"])) {
+  $upload_info = $_FILES["photo_file"];
+
+  if ($upload_info['error'] == UPLOAD_ERR_OK) {
+    $img_name = basename($upload_info["name"]);
+    $upload_ext = strtolower(pathinfo($img_name, PATHINFO_EXTENSION) );
+
+    if($upload_ext=="jpg" || $upload_ext=="jpeg" || $upload_ext=="png"){
+      //$user_id = $current_user;
+
+      $sql = "INSERT INTO images (img_name,img_ext,user_id) VALUES (:img_name,:img_ext,:user_id)";
+      $params = array(
+        ':img_name' => $img_name,
+        ':img_ext' => $upload_ext,
+        ':user_id' => $user_id
+      );
+      $result = exec_sql_query($db, $sql, $params);
+
+      if ($result) {
+        $img_id = $db->lastInsertId("id");
+        if (move_uploaded_file($upload_info["tmp_name"], IMG_UPLOADS_PATH . "$img_id.$upload_ext")){
+          array_push($messages, "Your file has been uploaded.");
+        }
+      } else {
+        array_push($messages, "Failed to upload file.");
+      }
+    }
+    else{
+      array_push($messages, "Failed to upload file. File was not a supported type.");
+    }
+  }
+  else {
+    array_push($messages, "Failed to upload file.");
+  }
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+</head>
+
+<body>
+  <?php include("includes/header.php");?>
+    <div id="content-wrap">
+    <title>Home - <?php echo $title;?></title>
+    <?php
+      if($delete_photo){
+        $sql = "DELETE FROM images WHERE id=:img_id;";
+        $params = array(
+          ':img_id' => $img_id,
+        );
+        $records = exec_sql_query($db, $sql, $params)->fetchAll();
+        foreach($records as $record){
+          unlink(IMG_UPLOADS_PATH.$record['img_name']);
+        }
+        echo "<h2>Photo was successfully deleted.</h2>";
+      }
+    ?>
+      <h1 id="article-title">Browse Gallery</h1>
+        <p>Browse gallery.</p>
+
+        <form id="uploadFile" action="gallery.php" method="post" enctype="multipart/form-data">
+      <ul>
+        <li>
+          <label>Upload File:</label>
+          <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MAX_FILE_SIZE; ?>" />
+          <input type="file" name="photo_file" required>
+        </li>
+        <li>
+          <button name="submit_upload" type="submit">Upload</button>
+        </li>
+      </ul>
+    </form>
+
+          <?php
+            $records = exec_sql_query($db, "SELECT * FROM images")->fetchAll(PDO::FETCH_ASSOC);
+            foreach($records as $record){
+              echo "<img src=\"" .IMG_UPLOADS_PATH. htmlspecialchars($record["id"].".".$record["img_ext"]) .
+              "\">" ."</a></div>";
+
+              //checks if user is owner and logged in before showing delete option
+              $sql = "SELECT * FROM images WHERE id = :img_id";
+              $params = array(
+                ':img_id' => $record["id"]
+              );
+              $records = exec_sql_query($db, $sql, $params)->fetchAll();
+              foreach($records as $record){
+                $owner_id = $record["user_id"];
+               echo htmlspecialchars($record["id"]);
+              //if($current_user) {
+              //if($current_user==$owner_id){?>
+
+                <form id="uploadFile" action=<?php echo "gallery.php" ?>
+                  method="post" enctype="multipart/form-data">
+                  <ul>
+                    <li>
+                      <label>Delete this photo:</label>
+                    </li>
+                    <li>
+                      <input type="submit" name="delete_photo" value="Delete">
+                    </li>
+                  </ul>
+                </form>
+              <?php }} ?>
+    </div>
+  </body>
+
+  <?php include("includes/footer.php");?>
+
+</html>
--- init/init.sql
+++ init/init.sql
@@ -1,12 +1,13 @@
 /* TODO: create tables */
 
 CREATE TABLE `images` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-  `img_name` TEXT NOT NULL,
+  `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`img_name`	TEXT NOT NULL,
+	`img_ext`	TEXT NOT NULL,
 	`user_id` INTEGER NOT NULL
 );
 
-INSERT INTO images (id, img_name, user_id, source) VALUES (1,'1.jpg',1);
+INSERT INTO images (id, img_name, img_ext, user_id) VALUES (1,'1.jpg','jpg',1);
 
 
 /* TODO: initial seed data */
