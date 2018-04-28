f15235f99bd6b4b381d24bec541fc142a4168034
Aileen <ac952@cornell.edu>
2018-04-28 15:33:16 -0400

    events

---
+++ events.php
@@ -0,0 +1,125 @@
+<?php include('includes/init.php');
+
+$current_page_id = "events";
+
+// Get the list of events from the database.
+$events = exec_sql_query($db, "SELECT DISTINCT event_name FROM events", NULL)->fetchAll(PDO::FETCH_COLUMN);
+
+if (isset($_POST["submit_insert"])) {
+  $event_name = filter_input(INPUT_POST, 'event_name', FILTER_SANITIZE_STRING);
+  $event_date = filter_input(INPUT_POST, 'event_date', FILTER_VALIDATE_STRING);
+  $event_time = filter_input(INPUT_POST, 'event_time', FILTER_VALIDATE_STRING);
+  $location = filter_input(INPUT_POST, 'location', FILTER_SANITIZE_STRING);
+  $description = filter_input(INPUT_POST, 'description', FILTER_VALIDATE_STRING);
+
+  if ( !in_array($event_name, $events) ) {
+    $invalid_review = TRUE;
+  }
+  if ($invalid_review) {
+    array_push($messages, "Failed to add event.");
+  } else {
+    $sql = "INSERT INTO events (event_name, event_date , event_time, location, description)
+    VALUES (:event_name, :event_date, :event_time, :location, :description)";
+    $params = array(
+      ':event_name' => $event_name,
+      ':event_date' => $event_date,
+      ':event_time' => $event_time,
+      ':location' => $location,
+      ':description' => $description
+    );
+
+    $result = exec_sql_query($db, $sql, $params);
+    if ($result) {
+      array_push($messages, "Your event has been added.");
+    } else {
+      array_push($messages, "Failed to add event.");
+    }
+  }
+}
+
+
+function print_event($record) {
+  ?>
+  <tr>
+    <td><?php echo htmlspecialchars($record["event_name"]);?></td>
+    <td><?php echo htmlspecialchars($record["event_date"]);?></td>
+    <td><?php echo htmlspecialchars($record["event_time"]);?></td>
+    <td><?php echo htmlspecialchars($record["location"]);?></td>
+    <td><?php echo htmlspecialchars($record["description"]);?></td>
+  </tr>
+  <?php
+}
+?>
+
+
+?>
+<!DOCTYPE html>
+<html lang ="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all" />
+  <title>Events</title>
+</head>
+<body>
+  <?php include("includes/header.php");?>
+  <div id="content-wrap">
+
+    <?php
+    $sql = "SELECT * FROM events";
+    $params = array();
+    $records = exec_sql_query($db, $sql, $params)->fetchAll();
+    ?>
+      <table>
+        <tr>
+          <th>Event Name</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Location</th>
+          <th>Description</th>
+        </tr>
+        <?php
+
+        foreach($records as $record) {
+          print_event($record);
+        }
+        ?>
+      </table>
+    ?>
+    <h2>Add an Event</h2>
+    <form id="addevent" action="events.php" method="post">
+      <ul>
+        <li>
+          <label>Event Name:</label>
+          <input type="text" name="event_name" required/>
+        </li>
+        <li>
+          <label>Date:</label>
+          <input type="text" name="event_date" required/>
+        </li>
+        <li>
+          <label>Time:</label>
+          <input type="text" name="event_time" required/>
+        </li>
+        <li>
+          <label>Location:</label>
+          <input type="text" name="location" required/>
+        </li>
+        <li>
+          <label>Description:</label>
+        </li>
+        <li>
+          <textarea name="comment" cols="40" rows="5"></textarea>
+        </li>
+        <li>
+          <button name="submit_insert" type="submit">Add Event</button>
+        </li>
+      </ul>
+    </form>
+  </div>
+
+  <?php include("includes/footer.php");?>
+
+</body>
+</html>
--- init/init.sql
+++ init/init.sql
@@ -10,4 +10,19 @@ CREATE TABLE `images` (
 INSERT INTO images (id, img_name, img_ext, user_id) VALUES (1,'1.jpg','jpg',1);
 
 
+
+CREATE TABLE `events`(
+  'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+  'event_name' TEXT NOT NULL,
+  'event_date' TEXT NOT NULL,
+  'event_time' TEXT NOT NULL,
+  'location' TEXT NOT NULL,
+  'description' TEXT NOT NULL
+);
+
+INSERT INTO events (id, event_name, event_date, event_time, location, description) VALUES (1,'djknkwe','05/28/18','4:00pm','Uris', 'gngkbml');
+INSERT INTO events (id, event_name, event_date, event_time, location, description) VALUES (2,'nwe','05/24/18','2:00pm','Olin', 'gngdkbml');
+INSERT INTO events (id, event_name, event_date, event_time, location, description) VALUES (3,'dkwe','05/18/18','1:00pm','Klarman', 'gngfweckbml');
+
+
 /* TODO: initial seed data */
