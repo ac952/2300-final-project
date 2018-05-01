93706658ef4fd7d163e24481e3550d3741782542
George Mao <gjm222@cornell.edu>
2018-05-01 10:14:15 -0400

    About page content

--- index.php
+++ index.php
@@ -17,6 +17,16 @@ $current_page_id = "index";
 <body>
   <?php include("includes/header.php");?>
   <h1 class = "page_header">About</h1>
+
+<!--Mission: Circle K International is comprised of college and university students who are responsible citizens and leaders sharing a lifelong commitment to community service worldwide. Circle K inspires people to better our world. Its motto, the same as Kiwanis International’s, is “We Build.” Circle K provides constructive opportunities for students to become involved on their campuses and in their communities through service work to others in need. Circle K members have the chance to work with fellow students, children in the community, and other adults in need of special programs.
+
+Circle K International, the largest university service leadership organization in the world, performs half a million hours of service each year. With more than 13,700 members in 19 nations, CKI is making a positive impact on the world every day.-->
+<!--Pledge: to uphold the Objects of Circle K International, to foster compassion and goodwill
+toward others through service and leadership, to develop my abilities and the abilities
+of all people, and to dedicate myself to the realization of mankind's potential."-->
+
+<!--# of national members: 13,770+, spans 17 nations-->
+<!--founded: 1936-->
 <!--Awards:
 NY District Club Spirit Award
 NY District Club Apparel Award
