a8dc5bb7f5bd552ce593d8fb890da32740a3a4d7
Shaina Verma <shainaverma@Shainas-MacBook.local>
2018-05-09 11:18:58 -0500

    First (NOT FINAL) draft of font changes

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $current_page_id = "index";
   <?php include("includes/header.php");?>
   <h1 class = "page_header">About</h1>
 
-<span class="about_subheadings">History</span><br>
+<span class="about_subheadings">History</span>
 <p>In 1936, the "Circle K House" at Washington State College was established by
   the Kiwanis Club of Pullman, Washington. Organized as a fraternity, Kappa Iota
   Phi served men who needed financial aid to attend college. Kiwanians also wanted
@@ -30,7 +30,7 @@ $current_page_id = "index";
   each year. With more than 13,700 members in 19 nations, CKI is making a positive
   impact on the world every day.</p>
 
-<span class="about_subheadings">Mission</span><br>
+<span class="about_subheadings">Mission</span>
 <p>Cornell Circle K is a part of Circle K International. Our organization is
   based around the 3 tenets of <span class="color_emphasis">service</span>,
   <span class="color_emphasis">leadership</span>, and <span class="color_emphasis">fellowship</span>.
@@ -43,13 +43,13 @@ $current_page_id = "index";
   work with fellow students, children in the community, and other adults in need
   of special programs.</p>
 
-<span class="about_subheadings">Pledge</span><br>
+<span class="about_subheadings">Pledge</span>
 <p>To uphold the Objects of Circle K International, to foster compassion and
   goodwill toward others through service and leadership, to develop our abilities
   and the abilities of all people, and to dedicate ourselves to the realization
   of mankind's potential.</p>
 
-<span class="about_subheadings">Philanthropy Initiatives</span><br>
+<span class="about_subheadings">Philanthropy Initiatives</span>
 <p>CKI raises funds for various causes. The major intitiative is "Focusing on the
   Future: Children" which aims to help children aged six to thirteen. This year,
   Cornell Circle K is focusing on the ways we can do small acts of service to
